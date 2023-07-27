0694647de6b904333a5c798a69b24891fca80bed
Rashedsafwan <rashedulalam203@gmail.com>
2023-07-27 23:18:40 +0600

    Final Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,4 @@ module.exports = {
   },
   plugins: [],
 }
-const btn = document.querySelector('button.mobile-menu-button');
-const menu = document.querySelector('.mobile-menu');
 
-btn.addEventListener('click', () => {
-menu.classList.toggle('hidden');
-});
